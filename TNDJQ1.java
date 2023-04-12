3b7c787df43531baa39764d48f6c3f242019510c
tlavksdmlskf <127850569+tlavksdmlskf@users.noreply.github.com>
2023-04-12 18:29:50 +0900

    Add files via upload

---
+++ TNDJQ1.java
@@ -0,0 +1,28 @@
+
+public class TNDJQ1 {
+
+	public static void main(String[] args) {
+		int a;
+		int b;
+		a=18;
+		b=99;
+		System.out.println("a는 "+a+"이고 b는 "+b+"입니다");
+		System.out.println("값을 바꾸겠읍니다~");
+		int c=a;
+		a=b;
+		b=c;
+		System.out.println("a는 "+a+"이고 b는 "+b+"입니다");
+		int x =1;
+		int y=2;
+		int z=3;
+		int aa;
+		aa=x;
+		x=y;
+		y=z;
+		z=aa;
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+	}
+
+}
