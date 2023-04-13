367ad2196a7f881c0688ebf470df6b1ea5eae33e
tlavksdmlskf <127850569+tlavksdmlskf@users.noreply.github.com>
2023-04-13 18:32:50 +0900

    Add files via upload

---
+++ Ab.java
@@ -0,0 +1,12 @@
+package a;
+
+public class Ab {
+
+	public static void main(String[] args) {
+		int a = 39;
+		String b = "";
+		b = (a%2==0) ? "Â¦¼ö":"È¦¼ö"; 
+		System.out.println(a+":"+b);
+	}
+
+}
