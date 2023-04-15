38cb0f40e8b841fef48cbb42fcfa7917fd75f9aa
tlavksdmlskf <127850569+tlavksdmlskf@users.noreply.github.com>
2023-04-15 23:50:11 +0900

    Add files via upload

---
+++ d.py
@@ -0,0 +1,9 @@
+i=input("휴대전화 번호 입력: ").split("-")[0]
+if i == "011":
+    print("당신은 SKT 사용자입니다.")
+elif i == "016":
+    print("당신은 SKT 사용자입니다.")
+elif i == "019":
+    print("당신은 LGU 사용자입니다.")
+elif i == "010":
+    print("알수없음")
