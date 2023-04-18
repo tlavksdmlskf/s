d45fc2dc584c921cc8d779e1ebc3d86e1b2f2762
tlavksdmlskf <127850569+tlavksdmlskf@users.noreply.github.com>
2023-04-18 17:25:09 +0900

    Update A.java

--- A.java
+++ A.java
@@ -9,7 +9,14 @@ public class A {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
 		System.out.println(num);
+		if (num<18) {
+			System.out.println(num+"은 18보다 작음ㅋ");
+		}else if (num>18) {
+			System.out.println(num+"은 18보다 큼");
+		}else {
+			System.out.println(num+"은 18이랑 같음");
 		
+		}
 
 	}
 
