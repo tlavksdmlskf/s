d5127e5d6a84248b95e0e4c02df859faa51f99ce
tlavksdmlskf <127850569+tlavksdmlskf@users.noreply.github.com>
2023-04-18 17:15:37 +0900

    Add files via upload

---
+++ A.java
@@ -0,0 +1,16 @@
+package aa;
+
+import java.util.Scanner;
+
+public class A {
+
+	public static void main(String[] args) {
+		System.out.println();
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		System.out.println(num);
+		
+
+	}
+
+}
