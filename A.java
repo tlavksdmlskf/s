f89d737e8273d2935e64e2f494c393f9017fe30b
tlavksdmlskf <127850569+tlavksdmlskf@users.noreply.github.com>
2023-04-18 17:33:07 +0900

    Update A.java

--- A.java
+++ A.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class A {
 
 	public static void main(String[] args) {
-		System.out.println();
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
 		System.out.println(num);
