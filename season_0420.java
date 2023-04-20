bfe7bb011e0957bc8649d69e883aacde30297c23
tlavksdmlskf <127850569+tlavksdmlskf@users.noreply.github.com>
2023-04-20 18:36:34 +0900

    Add files via upload

---
+++ season_0420.java
@@ -0,0 +1,27 @@
+package as;
+
+import java.util.Scanner;
+
+public class season_0420 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		switch (a) {
+		case 3: case 4: case 5:
+			System.out.println("봄");
+			break;
+		case 6: case 7: case 8:
+			System.out.println("여름");
+			break;
+		case 9: case 10: case 11:
+			System.out.println("가을");
+			break;
+		case 12:case 1: case 2: 
+			System.out.println("겨울");
+			break;
+		default :
+			System.out.println("잘못된 입력입니다");
+			break;		
+		}
+	}
+}
