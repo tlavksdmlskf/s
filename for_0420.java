4e4c34ee5a5f08b4c4cb1982a767c2337c0dd212
tlavksdmlskf <127850569+tlavksdmlskf@users.noreply.github.com>
2023-04-20 18:36:08 +0900

    Add files via upload

---
+++ for_0420.java
@@ -0,0 +1,69 @@
+package as;
+
+public class for_0420 {
+	public static void main(String[] args) {
+		int i = 1;
+//		if (i!=10) {
+//			System.out.println(i);
+//			i++;
+//		}
+//		if (i!=10) {
+//			System.out.println(i);
+//			i++;
+//		}
+//		if (i!=10) {
+//			System.out.println(i);
+//			i++;
+//		}
+//		if (i!=10) {
+//			System.out.println(i);
+//			i++;
+//		}
+//		if (i!=10) {
+//			System.out.println(i);
+//			i++;
+//		}
+//		if (i!=10) {
+//			System.out.println(i);
+//			i++;
+//		}
+//		if (i!=10) {
+//			System.out.println(i);
+//			i++;
+//		}
+//		if (i!=10) {
+//			System.out.println(i);
+//			i++;
+//		}
+//		if (i!=10) {
+//			System.out.println(i);
+//			i++;
+//		}
+//		if (i!=10) {
+//			System.out.println(i);
+//			i++;
+//		}
+//		if (i!=10) {
+//			System.out.println(i);
+//			i++;
+//		}
+//		i = 1;
+		for (i=1;i<=10;i++) {
+			System.out.println(i);
+		}
+		
+		for (int a=1;a<=10;a++) {
+			for (int b=1;b<=10;b++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for (int c=1;c<=5;c++) {
+			for (int d=1;d<=c;d++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		}
+	}
+}
