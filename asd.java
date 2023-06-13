f6ada75d3d20d0d53f5239efdfc7205a5fe6af19
tlavksdmlskf <127850569+tlavksdmlskf@users.noreply.github.com>
2023-06-13 18:18:52 +0900

    Add files via upload

---
+++ asd.java
@@ -0,0 +1,22 @@
+package asdf;
+
+public class asd {
+
+	public static void main(String[] args) {
+		
+		asdf_0613 p1 = new asdf_0613();
+		p1.hp = 58;
+		
+		p1.name = "김구라파덕";
+		
+		System.out.println(p1.name+"의 레벨은 : "+p1.lvl);
+		System.out.println("레벨업!!");
+		
+		p1.lvlup();
+		System.out.println(p1.name+"의 레벨은 : "+p1.lvl);
+		
+		
+
+	}
+
+}
---
+++ asdf_0613.java
@@ -0,0 +1,17 @@
+package asdf;
+
+public class asdf_0613 {
+	String name;		
+	int hp;
+	int atk;
+	int def;
+	int lvl = 1;
+	
+	void dont_cry() {
+		
+	}
+	void lvlup() {
+		lvl++;
+	}
+
+}
